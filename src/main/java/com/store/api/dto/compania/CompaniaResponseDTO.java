c06035a48a4ca3facaad69da72695f7a5aa6263a
eitan <eitanluc@gmail.com>
2026-02-01 20:59:58 -0300

    dtos de compania ya actualizacion de service y test

---
+++ src/main/java/com/store/api/dto/compania/CompaniaResponseDTO.java
@@ -0,0 +1,21 @@
+package com.store.api.dto.compania;
+
+public class CompaniaResponseDTO {
+    private Long id;
+    private String nombre;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}
---
+++ src/main/java/com/store/api/dto/compania/CompaniaUpdateDTO.java
@@ -0,0 +1,19 @@
+package com.store.api.dto.compania;
+
+public class CompaniaUpdateDTO {
+    private Long id;
+    private String nombre;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+}
